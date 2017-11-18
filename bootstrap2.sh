532aba5bea70fd73c75a42a55f86a761771fc69e
josephgunawan97 <joseph.gunawan97@gmail.com>
2017-11-18 18:29:52 +0700

    update

--- bootstrap2.sh
+++ bootstrap2.sh
@@ -1,6 +1,6 @@
 sudo apt-get install haproxy
 cd /etc/haproxy/
 sudo mv haproxy.cfg haproxy.cfg.old
-sudo wget https://raw.githubusercontent.com/josephgunawan97/vagrant_ansible_multi_server/master/haproxy.cfg
+sudo wget https://raw.githubusercontent.com/josephgunawan97/vagrant-uas/master/haproxy.cfg
 
 sudo service haproxy reload
